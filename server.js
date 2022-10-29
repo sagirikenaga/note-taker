da759acf9e4a1a1b65084a69f88569f551ef3177
Sagiri <sagiri.ikenaga@ryerson.ca>
2022-10-29 17:56:06 -0400

    added comments

---
+++ server.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const db = require('./db/db.json')
+var uuidv1 = require('uuidv1')
+
+const PORT = process.env.PORT || 3002;
+const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+app.use(express.static("public"));
+
+// gets the notes.html file 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// gets the db.json file and returns all saved notes
+app.get('/api/notes', (req, res) => {
+    const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
+    const parseNotes = JSON.parse(dataNotes);
+    res.json(parseNotes);
+});
+
+// saves new note to db.json file 
+app.post('/api/notes', (req, res) => {
+    const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
+    const parseNotes = JSON.parse(dataNotes);
+    req.body.id = uuidv1()
+    parseNotes.push(req.body);
+
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(parseNotes), "utf-8");
+    res.json("You have successfully added a note!");
+});
+
+//gets the index.html file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
